1f538eafb115f2cf3a85d81eac3db3a93fa3d8ea
Vladyslav Nhuien <vladyslavnh01@gmail.com>
2023-04-06 21:35:25 +0300

    Initial Commit

---
+++ BeerList-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerList-MVC
+//
+//  Created by Vladyslav Nhuien on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
